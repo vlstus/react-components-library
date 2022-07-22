3b54119859e8a19b555aebd1f56e56d409139d07
Vlad Stusiak <vlstus377@gmail.com>
2022-07-22 11:18:34 +0300

    rollup configured

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import {terser} from 'rollup-plugin-terser';
+import external from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import svg from 'rollup-plugin-svg';
+
+const packageJson = require('./package.json');
+
+export default {
+    input: 'src/index.tsx',
+    output: [
+        {
+            dir: packageJson.main,
+            format: 'cjs',
+            sourcemap: true,
+            name: 'react-component-lib'
+        },
+        {
+            dir: packageJson.module,
+            format: 'esm',
+            sourcemap: true
+        }
+    ],
+    plugins: [
+        external(),
+        resolve(),
+        commonjs(),
+        typescript({tsconfig: './tsconfig.json'}),
+        postcss(),
+        terser(),
+        svg()
+    ]
+}
