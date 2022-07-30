8e2ba2da9ef38eca1fcf542a54132ce3c19050d6
Vlad Stusiak <vlstus377@gmail.com>
2022-07-30 11:12:57 +0300

    select option identity is represented by id instead of value now. Assuming value may be non-unique

--- src/multiselect/Multiselect.tsx
+++ src/multiselect/Multiselect.tsx
@@ -22,7 +22,7 @@ class Multiselect extends React.Component<Readonly<MultiselectProperties>> {
     }
 
     private handleSelect(selectEvent: React.SyntheticEvent<HTMLSelectElement>) {
-        const selectedOption = this.props.availableOptions.find(option => option.value === selectEvent.currentTarget.value);
+        const selectedOption = this.props.availableOptions.find(option => option.id === Number(selectEvent.currentTarget.selectedOptions[0].dataset.id));
         const selectedOptions = [...this.props.selectedOptions.filter(option => option.id !== selectedOption?.id), ...selectedOption ? Array.of(selectedOption) : []];
         const remainingOptions = this.props.availableOptions.filter(availableOption => selectedOptions.every(selectedOption => selectedOption.id !== availableOption.id));
         this.props.onSelect({
@@ -38,7 +38,7 @@ class Multiselect extends React.Component<Readonly<MultiselectProperties>> {
                 <div className='selected-container'>
                     <label>Selected: </label>
                     {this.props.selectedOptions.map(option => {
-                        return <span className='selected-button-container'>
+                        return <span className='selected-button-container' key={option.id}>
                             <button className='selected-button'
                                     data-id={option.id} key={option.id}
                                     onClick={this.handleRemove}>{`${option.label} X`}</button></span>
@@ -49,7 +49,7 @@ class Multiselect extends React.Component<Readonly<MultiselectProperties>> {
                                 onChange={this.handleSelect}>
                         {this.props.isMultiSelect ? <option>Available:</option> : <option>---SELECT--</option>}
                             {this.props.availableOptions.map(option => {
-                                return <option className='available-option' key={option.id}
+                                return <option className='available-option' data-id={option.id} key={option.id}
                                                value={option.value}>{option.label}</option>
                             })}
                     </select></span></div>
